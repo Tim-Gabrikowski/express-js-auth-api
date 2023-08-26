c94a2b7c6eddceb9567ee43356765941722f71ec
Tim Gabrikowski <tim@gabrikowski.de>
2023-08-26 16:04:35 +0200

    add main route and configure express

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { inspect } from "util";
 // import all functions from the logger module to a single object
 import * as logger from "./logger.js";
 
@@ -8,9 +9,23 @@ dotenv.config();
 import Express from "express";
 const app = Express();
 
+// import and use cors
+import cors from "cors";
+app.use(cors());
+
+app.use(Express.json());
+
+// include main Router for main route
+import { mainRouter } from "./routes/main.js";
+app.use("", mainRouter);
+
 app.listen(process.env.PORT, () => {
 	logger.info(
 		"MAIN",
 		"Listening on: " + "http://localhost:" + process.env.PORT
 	);
+});
+
+process.on("uncaughtException", (error) => {
+	logger.critical("MAIN", inspect(error));
 });
